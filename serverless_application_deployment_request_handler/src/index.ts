80fafb46795f55af3361632c81fbf840870a69d2
Anurag Varma <panuragvarma123456789@gmail.com>
2024-06-18 20:59:40 +0530

    fix

--- serverless_application_deployment_request_handler/src/index.ts
+++ serverless_application_deployment_request_handler/src/index.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 const s3 = new S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KE,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     endpoint: "https://s3.amazonaws.com/"
 })
 
